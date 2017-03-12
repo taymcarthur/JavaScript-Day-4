ccaf6da4c9af121902676a146bb7e563772d3feb
Taylor McArthur <taydm.erek@gmail.com>
2017-03-11 17:17:06 -0800

    finished

--- homeworkex.js
+++ homeworkex.js
@@ -1,71 +1,22 @@
 $(document).ready(function(){
-	// $("#value").on('click', function(){
-	// 	var value1=$("#value1").val()
-	// 	$("#answer").val(Number(value1) + 
-	// 		)
-	// }
-  var result = 0;
-  var currentEntry = '0';
+  var content = ""
   
-  $('.button').on('click', function(evt) {
+
+  $('.button').on('click', function() {
     var buttonPressed = $(this).html();
-    console.log(buttonPressed);
-    
-    if (buttonPressed === "C") {
-      result = 0;
-      currentEntry = '0';
-    } else if (buttonPressed === "CE") {
-      currentEntry = '0';
-    } else if (buttonPressed === "back") {
-      //currentEntry = currentEntry.substring(0, currentEntry.length-1);
-    } else if (buttonPressed === "+/-") {
-      currentEntry *= -1;
-    } else if (buttonPressed === '.') {
-      currentEntry += '.';
-    } else if (isNumber(buttonPressed)) {
-      if (currentEntry === '0') currentEntry = buttonPressed;
-      else currentEntry = currentEntry + buttonPressed;
-    } else if (isOperator(buttonPressed)) {
-      prevEntry = parseFloat(currentEntry);
-      operation = buttonPressed;
-      currentEntry = '';
-    } else if(buttonPressed === '%') {
-      currentEntry = currentEntry / 100;
-    } else if (buttonPressed === 'sqrt') {
-      currentEntry = Math.sqrt(currentEntry);
-    } else if (buttonPressed === '1/x') {
-      currentEntry = 1 / currentEntry;
-    } else if (buttonPressed === 'pi') {
-      currentEntry = Math.PI;
-    } else if (buttonPressed === '=') {
-      currentEntry = operate(prevEntry, currentEntry, operation);
-      operation = null;
+    if(buttonPressed == "=") {
+    	console.log(content)
+    	$(".answerScreen").html(eval(content))
+
+    } else if (buttonPressed == "C") {
+    	$(".answerScreen").html("0");
+    	content="0";
+    } else {
+    	content += buttonPressed
+    	$(".answerScreen").html(content)
     }
-    
-    updateScreen(currentEntry);
-  });
-});
+   })
 
-updateScreen = function(displayValue) {
-  var displayValue = displayValue.toString();
-  $('.screen').html(displayValue.substring(0, 10));
-};
+ })
 
-isNumber = function(value) {
-  return !isNaN(value);
-}
 
-isOperator = function(value) {
-  return value === '/' || value === '*' || value === '+' || value === '-';
-};
-
-operate = function(a, b, operation) {
-  a = parseFloat(a);
-  b = parseFloat(b);
-  console.log(a, b, operation);
-  if (operation === '+') return a + b;
-  if (operation === '-') return a - b;
-  if (operation === '*') return a * b;
-  if (operation === '/') return a / b;
-}
-})
